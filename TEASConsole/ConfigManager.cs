ca2ed2f6a1431f8d3e9b9161257c4617fc14a8cd
Dan Sonntag <daniel.sonntag@outlook.com>
2023-01-29 17:13:19 +0100

    Reworked CLI options and implemented config manager

---
+++ TEASConsole/ConfigManager.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEASConsole
+{
+    /// <summary>
+    /// Class for handling TEASConsole bot configuration files
+    /// </summary>
+    internal class ConfigManager
+    {
+        /// <summary>
+        /// The ID of the Discord Guild that the bot should make itself available in
+        /// </summary>
+        public string GuildID { get; set; }
+
+        /// <summary>
+        /// The Bot Token of the Discord application that the bot should connect to
+        /// </summary>
+        public string BotToken { get; set; }
+
+        /// <summary>
+        /// An audio DeviceFriendlyName that should automatically be used on startup
+        /// </summary>
+        public string DefaultDeviceFriendlyName { get; set; }
+
+        /// <summary>
+        /// A Discord channel ID that the bot should automatically connect to on startup
+        /// </summary>
+        public string DefaultChannelID { get; set; }
+
+        /// <summary>
+        /// A list of admin users in the form 'Username#Discriminator' that the bot should accept commands from
+        /// </summary>
+        public List<string> AdminUsers { get; set; }
+
+        /// <summary>
+        /// A list of server roles that the bot should accept commands from
+        /// </summary>
+        public List<string> AdminRoles { get; set; }
+
+        /// <summary>
+        /// Initialises a new configuration based on a configuration file
+        /// </summary>
+        /// <param name="configFilePath">The path to an existing configuration file</param>
+        public ConfigManager(string configFilePath)
+        {
+            Parse(configFilePath);
+        }
+
+        /// <summary>
+        /// Initialises a new configuration
+        /// </summary>
+        /// <param name="guildID">The ID of the Discord Guild that the bot should make itself available in</param>
+        /// <param name="botToken">The Bot Token of the Discord application that the bot should connect to</param>
+        /// <param name="defaultDeviceFriendlyName">An audio device ID that should automatically be used on startup</param>
+        /// <param name="defaultChannelID">A Discord channel ID that the bot should automatically connect to on startup</param>
+        /// <param name="adminUsers">A comma-separated string of admin users in the form 'Username#Discriminator' that the bot should accept commands from</param>
+        /// <param name="adminRoles">A comma-separated string of server roles that the bot should accept commands from</param>
+        public ConfigManager(string guildID = "",
+                            string botToken = "",
+                            string defaultDeviceFriendlyName = "",
+                            string defaultChannelID = "",
+                            string adminUsers = "",
+                            string adminRoles = "")
+        {
+            GuildID = guildID;
+            BotToken = botToken;
+            DefaultDeviceFriendlyName = defaultDeviceFriendlyName;
+            DefaultChannelID = defaultChannelID;
+            AdminUsers = adminUsers.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+            AdminRoles = adminRoles.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+
+            Validate();
+        }
+
+        /// <summary>
+        /// <para>Parses a config file into the ConfigManager's attribute properties, and validats the resulting configuration afterwards.
+        /// A valid TEASConsole configuration file has the following structure (values in [] are placeholders, * marks an optional property):</para>
+        /// 
+        /// <para>GuildID=[Discord Guild ID where the bot is used]<br />
+        /// BotToken=[Application bot token to connect to]<br />
+        /// DefaultDevice=[A FriendlyName of an audio device the bot should use]*<br />
+        /// DefaultChannel=[Channel ID of a Discord channel that the bot connects to on startup]*<br />
+        /// AdminUsers=[Comma-separated list of Discord users that the bot accepts commands from]*<br />
+        /// AdminRoles=[Comma-separated list of server role names that the bot accepts commands from]*
+        /// </para>
+        /// </summary>
+        /// <param name="configFilePath">The path to the config file</param>
+        public void Parse(string configFilePath)
+        {
+            foreach (string optionLine in System.IO.File.ReadAllLines(configFilePath))
+            {
+                string[] option = optionLine.Split('=');
+                switch (option[0])
+                {
+                    case "GuildID":
+                        GuildID = option[1];
+                        break;
+                    case "BotToken":
+                        BotToken = option[1];
+                        break;
+                    case "DefaultDevice":
+                        DefaultDeviceFriendlyName= option[1];
+                        break;
+                    case "DefaultChannel":
+                        DefaultChannelID = option[1];
+                        break;
+                    case "AdminUsers":
+                        AdminUsers = option[1].Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+                        break;
+                    case "AdminRoles":
+                        AdminRoles = option[1].Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+                        break;
+                }
+            }
+            Validate();
+        }
+
+        /// <summary>
+        /// Validates the configuration and writes it to the specified file path. Overwrites existing files.
+        /// </summary>
+        /// <param name="configFilePath">The path to the (new) configuration file</param>
+        public void Write(string configFilePath)
+        {
+            Validate();
+            string[] options =
+            {
+                string.Join('=', "GuildID", GuildID),
+                string.Join('=', "BotToken", BotToken),
+                string.Join('=', "DefaultDevice", DefaultDeviceFriendlyName),
+                string.Join('=', "DefaultChannel", DefaultChannelID),
+                string.Join('=', "AminUsers", string.Join(',', AdminUsers.ToArray())),
+                string.Join('=', "AdminRoles", string.Join(',', AdminRoles.ToArray()))
+            };
+            File.WriteAllLines(configFilePath, options);
+        }
+
+        /// <summary>
+        /// Performs basic validation on the config object, to check whether required fields are set and valid data types.
+        /// Does not check whether Guild/Channel IDs or the bot token is actually available/accessible in Discord.
+        /// </summary>
+        /// <exception cref="Exceptions.ConfigValidationFailedException">Exception thrown if validation fails</exception>
+        public void Validate()
+        {
+            // Validate Guild ID
+            if (string.IsNullOrWhiteSpace(GuildID))
+                throw new Exceptions.ConfigValidationFailedException("The Guild ID is empty.");
+            else
+            {
+                try { Int64.Parse(GuildID); }
+                catch (Exception) { throw new Exceptions.ConfigValidationFailedException("The Guild ID is invalid."); }
+            }
+
+            // Validate Bot Token
+            if (string.IsNullOrWhiteSpace(BotToken))
+                throw new Exceptions.ConfigValidationFailedException("The bot token is empty.");
+            else
+            {
+                if (BotToken.Length != 59 || !BotToken.Contains('.'))
+                    throw new Exceptions.ConfigValidationFailedException("The bot token is invalid.");
+            }
+
+            // Validate channel ID if set
+            if (!string.IsNullOrWhiteSpace(DefaultChannelID))
+            {
+                try { Int64.Parse(DefaultChannelID); }
+                catch (Exception) { throw new Exceptions.ConfigValidationFailedException("The default channel ID is invalid."); }
+            }
+        }
+
+        public class Exceptions
+        {
+            public class ConfigValidationFailedException : Exception { public ConfigValidationFailedException(string message) : base(message) { } }
+        }
+    }
+}
--- TEASConsole/TEAS.cs
+++ TEASConsole/TEAS.cs
@@ -6,6 +6,8 @@ using Microsoft.Extensions.Logging;
 using Serilog;
 using Serilog.Core;
 using TEASLibrary;
+using System.Linq;
+using System.Linq.Expressions;
 
 namespace TEASConsole
 {
@@ -16,16 +18,33 @@ namespace TEASConsole
         /// </summary>
         public class Options
         {
-            [Option('t', "token", Default = "bottoken.txt", HelpText = "The Discord bot token or a path to a text file that contains the Discord bot token")]
+            [Value(0, Default = "botconfig.txt", HelpText = "Path to the TEASConsole config file. " +
+                "If none is found a new one will be generated, values can be pre-set through arguments. " +
+                "If a valid config file is found, other CLI arguments will override values found in the file, but are not saved.")]
+            public string ConfigFilePath { get; set; }
+
+            [Option('g', "guild", Required = false, HelpText = "The ID of a Discord guild the bot makes itself available to.")]
+            public string GuildID { get; set; }
+
+            [Option('t', "token", Required = false, HelpText = "The bot token of the Discord application TEAS will connect to.")]
             public string Token { get; set; }
 
-            [Option('d', "device", Default = "", HelpText = "Preselect an audio device by its friendly name")]
-            public string PreSeDeviceName { get; set; }
+            [Option('d', "device", Default = "", HelpText = "The friendly name of an audio device that will be used by TEAS.")]
+            public string AudioFriendlyName { get; set; }
 
-            [Option('a', "admin", Default = "", HelpText = "Specify a Discord user that the bot should accept commands from, in addition to server owners. Format: <Username>#<Discriminator>")]
-            public string AdminUserName { get; set; }
+            [Option('c', "channel", Default = "", HelpText = "The ID of a Discord channel that the bot will connect to on startup.")]
+            public string ChannelID { get; set; }
 
-            [Option('v', "verbose", Required = false, HelpText = "Enables debug messages")]
+            [Option("admin-names", Default = "", HelpText = "A comma-separated list of Discord users that the bot will accept commands from.")]
+            public string AdminUserNames { get; set; }
+
+            [Option("admin-roles", Default = "", HelpText = "A comma-separated list of server role names that the bot will accept commands from.")]
+            public string AdminRoles { get; set; }
+
+            [Option("new", Required = false, HelpText = "Ignores any existing config file and creates a new one in an interactive session.")]
+            public bool NewConfig { get; set; }
+
+            [Option("verbose", Required = false, HelpText = "Enables debug messages.")]
             public bool Verbose { get; set; }
         }
 
@@ -45,63 +64,194 @@ namespace TEASConsole
             if (Helpers.CheckUpdate(appVersion) == 1)
                 Log.Warning("A newer version of TEASConsole is available. Please download it from the Releases section on GitHub: https://github.com/dandln/TheEpicAudioStreamer/releases/");
 
-            // Parse command line options
-            string BotToken = "";
-            string AudioDeviceName = "";
-            string AdminUserName = "";
-            bool Verbose = false;
-            CommandLine.Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
+            // BEGINN PARSING OF COMMAND LINE OPTIONS
+            string configFile = "";
+            string guildID = "";
+            string botToken = "";
+            string audioDeviceName = "";
+            string channelID = "";
+            string adminUsers = "";
+            string adminRoles = "";
+            bool newConfig = false;
+            bool verbose = false;
+
+            var parseResult = Parser.Default.ParseArguments<Options>(args).WithParsed<Options>(o =>
             {
-                // Check if token file exists or validate passed bot token instead
-                try
+                configFile = o.ConfigFilePath;
+                guildID = o.GuildID;
+                botToken = o.Token;
+                audioDeviceName = o.AudioFriendlyName;
+                channelID = o.ChannelID;
+                adminUsers = o.AdminUserNames;
+                adminRoles = o.AdminRoles;
+                newConfig = o.NewConfig;
+                verbose = o.Verbose;
+            });
+            if (parseResult.Tag == ParserResultType.NotParsed)
+                return;
+
+            if (verbose)
+            {
+                logLevelSwitch.MinimumLevel = Serilog.Events.LogEventLevel.Debug;
+
+                // Log options parsed from the CLI if debug logs are enabled
+                string parsedConfigs = "Config parsed from CLI: ";
+                parsedConfigs += $"Config({configFile}) ";
+                if (!string.IsNullOrWhiteSpace(guildID))
+                    parsedConfigs += $"GuildID({guildID}) ";
+                if (!string.IsNullOrWhiteSpace(botToken))
+                    parsedConfigs += $"BotToken({botToken}) ";
+                if (!string.IsNullOrWhiteSpace(audioDeviceName))
+                    parsedConfigs += $"AudioDeviceName({audioDeviceName}) ";
+                if (!string.IsNullOrWhiteSpace(channelID))
+                    parsedConfigs += $"ChannelID({channelID}) ";
+                if (!string.IsNullOrWhiteSpace(adminUsers))
+                    parsedConfigs += $"AdminUsers({adminUsers}) ";
+                if (!string.IsNullOrWhiteSpace(adminRoles))
+                    parsedConfigs += $"AdminRoles({adminRoles}) ";
+                if (newConfig)
+                    parsedConfigs += "NewConfig(true)";
+                Log.Debug(parsedConfigs);
+            }
+
+            ConfigManager config = null;
+            if (newConfig)
+            {
+                // User wants to create a new config from scratch, ignore existing configuration and launch configuration assistant
+                /// TODO: Implement configuration assistant, for now quit programme
+                return;
+            }
+            else
+            {
+                if (File.Exists(configFile))
                 {
-                    BotToken = File.ReadAllText(o.Token);
-                }
-                catch (FileNotFoundException)
-                {
-                    if (o.Token.Length == 59 && o.Token.Contains('.'))
+                    // Conifg file exists, parse options
+                    Log.Debug("Parsing existing config file \"{0}\"", configFile);
+                    try
                     {
-                        BotToken = o.Token;
+                        config = new ConfigManager(configFile);
+                        Log.Debug("Valid config file found");
+
+                        // Config file valid, but check if one of the override options are set in the CLI, if so, apply them
+                        if (!string.IsNullOrWhiteSpace(guildID))
+                        {
+                            Log.Information("Guild ID {0} is set in CLI arguments, overriding config file", guildID);
+                            config.GuildID = guildID;
+                        }
+                        if (!string.IsNullOrWhiteSpace(botToken))
+                        {
+                            Log.Information("Bot token is set in CLI arguments, overriding config file");
+                            config.BotToken = botToken;
+                        }
+                        if (!string.IsNullOrWhiteSpace(audioDeviceName))
+                        {
+                            Log.Information("Audio device name {0} is set in CLI arguments, overriding config file", audioDeviceName);
+                            config.DefaultDeviceFriendlyName = audioDeviceName;
+                        }
+                        if (!string.IsNullOrWhiteSpace(channelID))
+                        {
+                            Log.Information("Channel id {0} is set in CLI arguments, overriding config file", channelID);
+                            config.DefaultChannelID = channelID;
+                        }
+                        if (!string.IsNullOrWhiteSpace(adminUsers))
+                        {
+                            Log.Information("Admin users \"{0}\" are set in CLI arguments, overriding config file", adminUsers);
+                            config.AdminUsers = adminUsers.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+                        }
+                        if (!string.IsNullOrWhiteSpace(adminRoles))
+                        {
+                            Log.Information("Admin roles \"{0}\" are set in CLI arguments, overriding config file", adminRoles);
+                            config.AdminRoles = adminRoles.Split(',', StringSplitOptions.RemoveEmptyEntries | StringSplitOptions.TrimEntries).ToList();
+                        }
+
+                        // Run one last check to see whether config is valid with the overriden options
+                        try
+                        {
+                            config.Validate();
+                        }
+                        catch (ConfigManager.Exceptions.ConfigValidationFailedException ex)
+                        {
+                            Log.Fatal($"Error encountered while validating config overrides: {ex.Message} " +
+                                $"Check CLI arguments.");
+                            return;
+                        }
                     }
-                    else
+                    catch (Exception ex)
                     {
-                        Log.Error("Bot token file not found and no valid token given");
+                        Log.Fatal($"Error ecountered while trying to parse existing configuration file: {ex.Message} " +
+                            $"Try to fix the configuration or run '--new' to create a new one.");
                         return;
                     }
                 }
+                else
+                {
+                    // Config file does not exist, check if required parameters are set through CLI
+                    if (!string.IsNullOrWhiteSpace(guildID) && !string.IsNullOrWhiteSpace(botToken))
+                    {
+                        Log.Debug("Config file \"{0}\" not found, but required options were given via CLI, creating config...", configFile);
+                        try
+                        {
+                            config = new ConfigManager(guildID, botToken, audioDeviceName, channelID, adminUsers, adminRoles);
+                            config.Write(configFile);
+                        }
+                        catch (ConfigManager.Exceptions.ConfigValidationFailedException ex)
+                        {
+                            Log.Fatal($"Given required configuration options are invalid: {ex.Message} " +
+                                $"Check CLI arguments.");
+                            return;
+                        }
+                        catch (Exception ex)
+                        {
+                            Log.Fatal($"An error occured when trying to save the new configuration: {ex.Message}");
+                            return;
+                        }
+                    }
+                    else
+                    {
+                        // Config file was not found, and required options were not set via CLI, creating a new config in the configuration assistant
+                        if (File.Exists("bottoken.txt"))
+                        {
+                            // An older bottoken file was found, probably the user has updated from an old TEASConsole version
+                            Log.Warning("Config file not found, but found an old bottoken.txt file. " +
+                                "Have you recently updated from an earlier TEASConsole version? " +
+                                "Check https://github.com/dandln/TheEpicAudioStreamer#migrating-from-earlier-versions for more information. " +
+                                "Launching configuration assistant...");
+                        }
+                        else
+                        {
+                            Log.Warning("Config file was not found, and required options were not set via command line. Launching configuration assistant...");
+                        }
 
-                // Parse options
-                AudioDeviceName = o.PreSeDeviceName;
-                AdminUserName = o.AdminUserName;
-                Verbose = o.Verbose;
-            });
-
-            if (Verbose)
-                logLevelSwitch.MinimumLevel = Serilog.Events.LogEventLevel.Debug;
-            
-            if (BotToken == "")
-            {
-                Log.Fatal("Empty bot token. Exiting...");
-                return;
+                        /// TODO: Implement configuration assistant, for now quit programme
+                        return;
+                    }
+                }
             }
+            // END PARSING OF COMMAND LINE OPTIONS
 
             // Get an audio device from the user
             MMDevice AudioDevice;
-            AudioDevice = Helpers.SelectDevice(AudioDeviceName);
+            AudioDevice = Helpers.SelectDevice(config.DefaultDeviceFriendlyName);
             while (AudioDevice == null)
             {
                 Console.WriteLine("No valid audio device selected. Try again.\n");
-                AudioDevice = Helpers.SelectDevice(AudioDeviceName);
+                AudioDevice = Helpers.SelectDevice(audioDeviceName);
             }
             Log.Information("Chosen audio device is {0}", AudioDevice.DeviceFriendlyName);
 
-            if (AdminUserName != "")
-                Log.Information("The bot will accept commands from user {0}", AdminUserName);
+            if (config.AdminUsers.Count != 0)
+                Log.Information("The bot will accept commands from users {0}", config.AdminUsers.ToString());
 
             // When all options and configurations are parsed, create a new bot object and run it.
             var logFactory = new LoggerFactory().AddSerilog();
-            Bot bot = new(BotToken, logFactory, AdminUserName, AudioDevice, Verbose);
+            Bot bot = new(config.BotToken, logFactory, "", AudioDevice, verbose);
             bot.Connect().GetAwaiter().GetResult();
+
+            /// TODO: Create compatibility, between TEASLibrary and the new configuration formats
+            /// - Make use of admin users and admin roles (admin user left blank above)
+            /// - Connect directly to a specific Guild
+            /// ...
+
         }
     }
 }
